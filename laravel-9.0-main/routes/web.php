025a818b024e12f73f34379ac0f176b4b18e58dc
a212153ma <1hiawmushtaqahmad@gmail.com>
2025-03-04 17:36:42 +0100

    add only url's with desc

--- laravel-9.0-main/routes/web.php
+++ laravel-9.0-main/routes/web.php
@@ -16,3 +16,37 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// after logging the user will go to the home page of app
+// this is main page containing all info e:g films, series etc
+Route::get('/home', function () {
+    return view('home');
+});
+
+// this route will only display latest films only
+Route::get('/films', function () {
+    return view('filmList');
+});
+
+// After clicking in one film the route will change and take the id/name of that film
+// this route will responsible to show the film with their details
+Route::get('/films/SpiderMan', function () {
+    return view('spiderMan')
+});
+
+// Route for displaying the series lists
+Route::get('/series', function () {
+    return view('seriesList');
+});
+
+// After click in one series the routes will be change
+// and if sereies have episodes the user if click in any other episode
+// the route will change also
+Route::get('/series/name/episode1', function () {
+    return view('Berlin');
+});
+
+// Route to see myList/Favourite
+Route::get('/myList', function () {
+    return view('favouriteList');
+});
