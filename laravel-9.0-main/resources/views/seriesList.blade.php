df15ab2ec2458fef97543282bd464a649259e079
a212153ma <a212153ma@h12.informatica.escoladeltreball.org>
2025-03-05 20:08:36 +0100

    emp lista de series

---
+++ laravel-9.0-main/resources/views/seriesList.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.disenyo')
+@section('content')
+<div class="content">
+    <h1>Series List's</h1>
+    <div class="card" style="width: 18rem;">
+        <img src="{{ asset('image/berlin.jpg') }}" class="card-img-top" alt="Berlin">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</div>
+@endsection
