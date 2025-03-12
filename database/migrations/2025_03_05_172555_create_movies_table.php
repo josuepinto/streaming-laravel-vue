4637245753aaf232c673e19e4934602488aa8f05
a212394jp <a212394jp@h11.informatica.escoladeltreball.org>
2025-03-12 17:27:53 +0100

    Subiendo todo el código actualizado a la rama 11

---
+++ database/migrations/2025_03_05_172555_create_movies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->year('year');
+            $table->string('genre');
+            $table->string('image'); // URL o nombre del archivo
+            $table->string('video_url'); // URL del video
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+};
